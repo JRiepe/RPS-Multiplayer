0b208ede4bafa9b2efea3a6dcda0ed377b8f2155
John D. Riepe <JohnD.Riepe@danielles-mbp-2.attlocal.net>
2016-05-18 15:51:01 -0400

    changed to a three firebase system to counteract my lack of intelligence

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,124 +6,192 @@
 $(document).ready(function(){
             
 
-            var url = "https://jriepeweek07.firebaseio.com/";
-            var gameData = new Firebase(url);
+            var url1 = "https://jriepe-rps-players.firebaseio.com/";
+            var url2 = "https://jriepe-rps-turns.firebaseio.com/";
+            var url3 = "https://jriepe-rps-comments.firebaseio.com/";
 
-            // Initial Values
+            var gameData = new Firebase(url1);
+            var gameTurns = new Firebase(url2);
+            var gameComments = new Firebase(url3);
+            
+            var pSnapshot;
+            var tSnapshot;
+            var cSnapshot;
 
-            var snapshot;
-            var comment;
+            
             var yourName;
             var yourPlayer;
             var opponentName;
+            var p1Num;
+            var p2Num;
             var p1Name;
             var p2Name;
-
             var p1Wins = 0;
             var p2Wins = 0;
             var p1Losses = 0;
             var p2Losses = 0;
-
-            var p1Comment;
-            var p2Comment;
-            var localData;
+            var p1Choice = "";
+            var p2Choice = "";
+            var playerTurn = 1;
+            var userComments = "";
+            
+            var localData = "";
 
             
-
-            gameData.orderByChild("player").equalTo(1).on("child_added", function(snapshot) {
             
-                p1Name = snapshot.val().name;
-                p1Wins = snapshot.val().wins;
-                p1Losses = snapshot.val().losses;
-                $('#p1Info').html('<br>' + p1Name + '<br>Wins: ' + p1Wins + '<br>Losses: ' + p1Losses);
+
+            gameData.orderByChild("pnumber").equalTo(1).on("child_added", function(pSnapshot) {
+                
+                p1Num = pSnapshot.val().pnumber;
+                console.log('p1Num: '+p1Num);
+                p1Name = pSnapshot.val().name;
+
+                p1Wins = pSnapshot.val().wins;
+                
+                p1Losses = pSnapshot.val().losses;
+                
+                p1Choice = pSnapshot.val().choice;
+                
+                $('#p1Name').html('<br>' + p1Name);
+                $('#p1Buttons').html('');
+                $('#p1Score').html('<br>Wins: ' + p1Wins + '  Losses: ' + p1Losses);
+
+
+
             });
-            gameData.orderByChild("player").equalTo(2).on("child_added", function(snapshot) {
+            gameData.orderByChild("pnumber").equalTo(2).on("child_added", function(pSnapshot) {
                 $("#nameBox").html("");
-                p2Name = snapshot.val().name;
-                p2Wins = snapshot.val().wins;
-                p2Losses = snapshot.val().losses;
-                $('#p2Info').html('<br>' + p2Name + '<br>Wins: ' + p2Wins + '<br>Losses: ' + p2Losses);
+                console.log('Snap: '+pSnapshot.val())
+                p2Num = pSnapshot.val().pnumber;
+              
+                p2Name = pSnapshot.val().name;
+
+                p2Wins = pSnapshot.val().wins;
+                
+                p2Losses = pSnapshot.val().losses;
+
+                p2Choice = pSnapshot.val().choice;
+
+                $('#p2Name').html('<br>' + p2Name);
+                $('#p2Buttons').html('');
+                $('#p2Score').html('<br>Wins: ' + p2Wins + '  Losses: ' + p2Losses);
+
             });
                 
                 
-             gameData.on("value", function(snapshot) {   
-                console.log(snapshot.val());
-                localData = snapshot.val();
+            gameData.on("value", function(pSnapshot) {   
+                console.log(pSnapshot.val());
+                localData = pSnapshot.val();
 
             }, function (errorObject) {
                 //console.log("The read failed: " + errorObject.code);
             });
 
 
-            $("#addPlayer").on("click", function() {
+            $(document).on("click", "#addPlayer", function() {
                 yourName = $('#nameInput').val().trim();
                 
-                //var a = localData;
-                //var b = localData.numChildren();
-                //var c = 
-                //console.log('a: ' + a);
-                //console.log('b: ' + b);
-                //console.log('c: ' + c)
                
                 if (localData === null) {
                     yourPlayer = 1;
                     p1Name = yourName;
                     //console.log('p1Name: ' + p1Name);
-                    gameData.push({
-                        player: yourPlayer,
-                        name: p1Name,
-                        wins: p1Wins,
-                        losses: p1Losses
-                         
+                    
+                    gameTurns.set({
+                        turn: playerTurn
+                        
                     });
+
+                    gameComments.push({
+                        comments: userComments
+                    });
+                    
+                    gameData.push({
+                            pnumber: yourPlayer,
+                            name: p1Name,
+                            wins: p1Wins,
+                            losses: p1Losses,
+                            choice: p1Choice
+                        
+                     });    
+                    
+                    $("#nameBox").html("");
                     //console.log(ref.key());
                     //$('#p1Info').html('<br>' + p1Name + '<br>Wins: ' + p1Wins + '<br>Losses: ' + p1Losses);
                     return false;
                 }
-                else if (localData !== null && localData.player !== 2) {
+                else if (localData !== null && localData.pnumber != 2) {
                     yourPlayer = 2;
                     p2Name = yourName;
                     //console.log('p2Name: ' + p2Name);
-                    gameData.push({
-                        player: yourPlayer, 
-                        name: p2Name,
-                        wins: p2Wins,
-                        losses: p2Losses
-                          
+                    gameTurns.set({
+                        turn: playerTurn
+                        
                     });
+
+                    gameComments.push({
+                        comments: userComments
+                    });
+                    
+                    gameData.push({
+                            pnumber: yourPlayer,
+                            name: p2Name,
+                            wins: p2Wins,
+                            losses: p2Losses,
+                            choice: p2Choice
+                        
+                     });    
                     //$('#p2Info').html('<br>' + p2Name + '<br>' + 'Wins: ' + p2Wins + 'Losses: ' + p2Losses);
                 }
-                
-                else {
-                    alert("I'm sorry, the game is full. Please try again later!");
-                } 
-
-
-
-              // Alert User and reset the counter
-              
-
+                else    {
+                    alert('Sorry, already two players. Please try again later!')
+                }
 
                 return false;
 
               
             });
 
-            // Whenever a user clicks the restart button
-            $("#resetButton").on("click", function() {
+            // on click press rock button
+            $(document).on("click", "#rockButton", function() {
+                if (playerTurn === 1 && yourPlayer === 1) {
 
-              // clear out saved data
-                    gameData.set(null);
-                    $('#p1Info').html('');
+                }
+            }); // end $("#rockButton").on("click"
+            
+            // on click press paper button
+            $(document).on("click", "#paperButton", function() {
+
+            }); // end $("#paperButton").on("click"
+
+            // on click press scissors button
+            $(document).on("click", "#scissorsButton", function() {
+
+            }); // end $("#scissorsButton").on("click"
+
+
+            // Whenever a user clicks the restart-reset button
+            $(document).on("click", "#resetButton", function() {
+                    
+                    //gameData.orderByChild("name").equalTo(yourName).set(null);
+                        
+
+            
+                    //}
+                    // clears firebase data
+                    gameData.set(null); 
+                    location.reload();
+                    // below redraws name input form to play again!
+                   /* $('#p1Info').html('');
                     $('#p2Info').html('');
                     $("#nameBox").html('<form id="nameForm">');
                     $("#nameBox").append('<label for="nameInput">Enter your Name: </label>');
                     $("#nameBox").append('<input type="text" id="nameInput"><br><br>');
                     $("#nameBox").append('<input id="addPlayer" type="submit" value="Enter">');
-                    $("#nameBox").append('</form>');
-             }) 
-            // End logic with Firebase
-
+                    $("#nameBox").append('</form>'); */
+             
+             }) // end $("#resetButton").on("click"...
+            
  
 
 
